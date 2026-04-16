b099389492f450d91b097d9ab90dd6839feb6548
pOOM yANG <ZZOMGE@gmail.com>
2026-04-16 14:24:20 +0700

    .

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: new Date(),
     changeFrequency: 'monthly' as const,
     priority: 0.8,
-  }))
+  }))  
 
   return [
     {
